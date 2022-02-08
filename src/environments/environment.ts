68bd312a73bbfb069c8fb71ecc5e834fc558ab0c
François Chatelier <francois.chatelier@gmail.com>
2022-02-08 16:47:33 +0100

    initiation à Angular et Bootstrap

---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+    firebase: {
+    apiKey: "AIzaSyBtUfwNQWnfe9utQ-8gcbBnYxaZL7xOB50",
+    authDomain: "book-36a1d.firebaseapp.com",
+    projectId: "book-36a1d",
+    storageBucket: "book-36a1d.appspot.com",
+    messagingSenderId: "392470893224",
+    appId: "1:392470893224:web:4c3618d93e0cf7a810689d"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
